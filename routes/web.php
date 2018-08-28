96fa411e14a00e7aaa689639db5ee78c779d060d
Myo Thiha <myothiha576@gmail.com>
2018-08-29 01:32:11 +0630

    Webhook Setup

--- routes/web.php
+++ routes/web.php
@@ -11,6 +11,45 @@
 |
 */
 
+
+use Illuminate\Http\Request;
+
 Route::get('/', function () {
     return view('welcome');
+});
+
+Route::get('/webhook', function (Request $request) {
+//    dd($request->all());
+    // Your verify token. Should be a random string.
+    $VERIFY_TOKEN = "test_token";
+
+    // Parse the query params
+    $mode = $request->hub_mode;
+    $token = $request->hub_verify_token;
+    $challenge = $request->hub_challenge;
+
+    // Checks if a token and mode is in the query string of the request
+    if ($mode && $token) {
+        // Checks the mode and token sent is correct
+        if ($mode == "subscribe" && $token == $VERIFY_TOKEN) {
+            // Responds with the challenge token from the request
+//            echo 'WEBHOOK_VERIFIED';
+            dd($challenge);
+        }
+    }
+});
+
+Route::post('/webhook', function (Request $request) {
+//    dd($request->all());
+
+    // Checks this is an event from a page subscription
+    if ($request->object == 'page') {
+        // Iterates over each entry - there may be multiple if batched
+        foreach ($request->entry as $entry_item) {
+            // Gets the message. entry.messaging is an array, but
+            // will only ever contain one message, so we get index 0
+            $webhook_event = $entry_item["messaging"][0]["message"];
+            dd($webhook_event);
+        }
+    }
 });
